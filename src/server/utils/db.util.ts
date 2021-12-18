24d36d86e6e5aea894e7fdb4dad5470359149b77
Akshay Priyadarshi <akshayp1904@outlook.com>
2021-12-18 09:49:26 +0530

    Updated db.util.ts

--- src/server/utils/db.util.ts
+++ src/server/utils/db.util.ts
@@ -6,8 +6,8 @@ import { getEnv } from './env.util'
  * @author Akshay Priyadarshi <akshayp1904@outlook.com>
  */
 export async function ensureDatabaseConnection(): Promise<void> {
-	const DB_URL = getEnv('DB_URL') as string
 	try {
+		const DB_URL = getEnv('DB_URL') as string
 		await mongoose.connect(DB_URL).then(() => {
 			console.log('🚀 Database connection ensured')
 		})
